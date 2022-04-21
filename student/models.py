575e0087be25346c138acdc1a918497092cf5739
AliasgarBohra <alibohra752@gmail.com>
2022-04-21 17:56:32 +0530

    triopush'

---
+++ student/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Student(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    profile_pic= models.ImageField(upload_to='profile_pic/Student/',null=True,blank=True)
+    age = models.IntegerField(max_length=3,null=False,default=0)
+   
+    @property
+    def get_name(self):
+        return self.user.first_name+" "+self.user.last_name
+    @property
+    def get_instance(self):
+        return self
+    def __str__(self):
+        return self.user.first_name
